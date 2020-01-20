469c7139ce07ac64411ea1b31688376e9335e850
Xin Liu <xinliu253@yahoo.com>
2020-01-19 23:59:16 -0500

    adding binary numbers

--- practice/Binary.java
+++ practice/Binary.java
@@ -33,10 +33,13 @@ public class Binary {
 		print("DecimalToBinaryIntoNumberNonString: " + 10 + " is " + NonString);
 		int NonString1 = DecimalToBinaryIntoNumberNonString(1);
 		print("DecimalToBinaryIntoNumberNonString: " + 1 + " is " + NonString1);
-
-	
-
 		
+		print("TwoBinarySum: 111 and 100 is: " + TwoBinarySum(111,100));
+		print("TwoBinarySum: 111111 and 100 is: " + TwoBinarySum(111111,100));
+		
+		print("TwoBinarySum taking Strings as input: 1111, 1000 sum is " + TwoBinarySum("1111","1000"));
+		print("TwoBinarySum taking Strings as input: 1111, 111 sum is " + TwoBinarySum("1111","111"));
+
 	}
 	public static void print(Object o) {
 		System.out.println(o);
@@ -86,6 +89,54 @@ public class Binary {
 		int result = 0;
 		for(int i = 0; i < ls.size(); i++) {
 			result += ls.get(i) * Math.pow(10,i);
+		}
+		return result;
+	}
+	
+	//adding two binary numbers
+	public static int TwoBinarySum(int num1, int num2) {
+		String str1 = num1 + "";
+		String str2 = num2 + "";
+		int len1 = str1.length();
+		int len2 = str2.length();
+		int carry = 0;
+		String temp = "";
+		int max = Math.max(len1, len2);
+		for(int i = 0; i < max; i ++){
+			int p = i < len1 ? str1.charAt(len1-1-i) - '0' : 0;
+			int q = i < len2 ? str2.charAt(len2-1-i) - '0' : 0;
+			int sum = q + p + carry;
+			carry = sum/2;
+			temp += sum%2;
+		}
+		if(carry == 1) {
+			temp += "1";
+		}
+		String result = "";
+		for(int i = temp.length()-1; i >= 0; i--) {
+			result += temp.charAt(i);
+		}
+		return Integer.parseInt(result);
+	}
+	public static String TwoBinarySum(String num1, String num2) {
+		int len1 = num1.length();
+		int len2 = num2.length();
+		int max = Math.max(len1, len2);
+		String temp = "";
+		int carry = 0;
+		for(int i = 0; i < max; i++) {
+			int p = i < len1 ? num1.charAt(len1-1-i) - '0' : 0;
+			int q = i < len2 ? num2.charAt(len2-1-i) - '0' : 0;
+			int num = q + p + carry;
+			carry = num/2;
+			temp += num%2;
+		}
+		if(carry == 1) {
+			temp += "1";
+		}
+		String result = "";
+		for(int i = temp.length()-1; i >= 0; i --) {
+			result += temp.charAt(i);
 		}
 		return result;
 	}
