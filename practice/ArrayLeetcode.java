9debb7860d262311d54e10943f180d56a8b1c414
Xin Liu <xinliu253@yahoo.com>
2020-01-31 20:53:23 -0500

    arrays

--- practice/ArrayLeetcode.java
+++ practice/ArrayLeetcode.java
@@ -5,9 +5,19 @@ import java.util.*;
 public class ArrayLeetcode {
 	
 	public static void main(String []args) {
-		int a = 1;
-		System.out.println(++a);
+		char a = a;
+		int b = 90;
 		System.out.println(a);
+		System.out.println(b);
+		//gave number, turn it to char
+		int a1 = a-'a';
+		System.out.println(a1);
+		//[a,b,c,d,e,f,g,..];
+		//[0,1,2,3,4,5...]
+		char b1 = (char)b;
+		char a2 = a;
+		System.out.println(a2);
+		//System.out.println(b1);
 	}
 	
 	/*Given an array A of integers, return true if and only if it is a valid mountain array.
@@ -555,5 +565,67 @@ points[i].length == 2
 	            }
 	        }
 	                return true;
+	    }
+	   /*Given an array A of integers, return true if and only if we can partition 
+	    * the array into three non-empty parts with equal sums.
+	Formally, we can partition the array if we can find indexes i+1 < j with (A[0] + A[1] + 
+	... + A[i] == A[i+1] + A[i+2] + ... + A[j-1] == A[j] + A[j-1] + ... + A[A.length - 1])
+	Example 1:
+	Input: [0,2,1,-6,6,-7,9,1,2,0,1]
+	Output: true
+	Explanation: 0 + 2 + 1 = -6 + 6 - 7 + 9 + 1 = 2 + 0 + 1
+	Example 2:
+	
+	Input: [0,2,1,-6,6,7,9,-1,2,0,1]
+	Output: false
+	Example 3:
+	
+	Input: [3,3,6,5,-2,2,5,1,-9,4]
+	Output: true
+	Explanation: 3 + 3 = 6 = 5 - 2 + 2 + 5 + 1 - 9 + 4
+	 
+	
+	Note:
+	
+	3 <= A.length <= 50000
+	-10000 <= A[i] <= 10000*/
+	    
+	    public static boolean canThreePartsEqualSum(int[] A) {
+	        int count = 0;
+	        int total = 0;
+	        for(int i : A){
+	            total += i;
+	        }
+	        if(total % 3 != 0)
+	            return false;
+	        int sum = total/3;
+	        int index = 0;
+	        w:while(index < A.length){
+	            int temp = A[index];
+	            while(temp != sum){
+	                temp += A[++index];
+	                if(index >= A.length-1)
+	                    break w;
+	            }
+	            count ++;
+	            index ++;
+	        }  
+	        return count == 3;
+	    }
+	    public static boolean canThreePartsEqualSumOneLoop(int[] A) {
+	        int count = 0;
+	        int total = 0;
+	        for(int i : A){
+	            total += i;
+	        }
+	        if(total % 3 != 0)
+	            return false;
+	        int sum = total/3;
+	        
+	        return count == 3;
+	    }
+	    
+	    public static List<String> commonChars(String[] A) {
+	        
 	    }
 }
