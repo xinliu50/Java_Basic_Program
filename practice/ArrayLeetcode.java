dacd790cb67a18f1e8cdec425d01550b6794ba0c
Xin Liu <xinliu253@yahoo.com>
2020-02-06 12:02:44 -0500

    array practice

--- practice/ArrayLeetcode.java
+++ practice/ArrayLeetcode.java
@@ -22,6 +22,8 @@ public class ArrayLeetcode {
 		print(a);
 		
 		print(temp);
+		var map = new HashMap();
+		
 	}
 	public static void print(int[]a) {
 		for(int i = 0; i < a.length; i ++) 
@@ -354,7 +356,7 @@ Example 2:
 
 Input: points = [[3,2],[-2,2]]
 Output: 5
-Constraints:
+Constraints: 
 
 points.length == n
 1 <= n <= 100
@@ -1199,6 +1201,49 @@ matrix[i][j] is either 0 or 1.*/
 	        Collections.reverse(ls);
 	        Integer[] a = ls.toArray(new Integer[0]);
 	        int[] result = Arrays.stream(a).mapToInt(i->i).toArray();
+	        return result;
+	    }
+	    /*Given an array of integers arr, replace each element with its rank.
+		The rank represents how large the element is. The rank has the following rules:
+		Rank is an integer starting from 1.
+		The larger the element, the larger the rank. If two elements are equal, their rank 
+		must be the same.
+		Rank should be as small as possible.
+		Example 1:
+		Input: arr = [40,10,20,30]
+		Output: [4,1,2,3]
+		Explanation: 40 is the largest element. 10 is the smallest. 20 is the second smallest. 
+		30 is the third smallest.
+		Example 2:
+		Input: arr = [100,100,100]
+		Output: [1,1,1]
+		Explanation: Same elements share the same rank.
+		Example 3:
+		Input: arr = [37,12,28,9,100,56,80,5,12]
+		Output: [5,3,4,2,8,6,7,1,3]
+		 
+		
+		Constraints:
+		
+		0 <= arr.length <= 105
+		-109 <= arr[i] <= 109*/
+	    public static int[] arrayRankTransform(int[] arr) {
+	        int[] temp = new int[arr.length];
+	        Arrays.sort(temp);
+	        var map = new HashMap<Integer,Integer>();
+	        int index = 1;
+	        for(int i = 0 ; i < temp.length; i ++){
+	            if(map.containsKey(temp[i])){
+	                 map.put(temp[i],--index);     
+	            }else{
+	                 map.put(temp[i],index);     
+	            } 
+	            index ++;
+	        }
+	        int[] result = new int[arr.length];
+	        for(int i = 0; i < arr.length; i ++){
+	            result[i] = map.get(arr[i]);
+	        }
 	        return result;
 	    }
 }
